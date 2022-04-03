0619bbc6685dc04290a1f985db228f04fb6370e2
MadisonLucey33 <101581514+MadisonLucey33@users.noreply.github.com>
2022-04-03 00:12:43 -0400

    here

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -140,7 +140,7 @@ const PostCard = ({ post }) => {
 
   };
 
-  return (
+  return ( // bg-pink-200 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 sm:bg-red-600 md:bg-gray-600 lg:bg-yellow-300 xl:bg-green-500 2xl:bg-cyan-400 3xl:bg-indigo-600 4xl:bg-violet-800
     <div className="bg-pink-200 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8 sm:bg-red-600 md:bg-gray-600 lg:bg-yellow-300 xl:bg-green-500 2xl:bg-cyan-400 3xl:bg-indigo-600 4xl:bg-violet-800">
       
       {/* <div className="relative shadow-md inline-block w-full h-60 lg:h-80 mb-6">
@@ -161,7 +161,7 @@ const PostCard = ({ post }) => {
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
-        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto lg:mr-8 items-center">
           <Image
             unoptimized
             loader={grpahCMSImageLoader}
@@ -177,7 +177,7 @@ const PostCard = ({ post }) => {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
           </svg>
-          <span className="align-middle"> { getUpdatedDate() }</span>
+          <span className="align-middle "> { getUpdatedDate() }</span>
         </div>
       </div>
       <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
